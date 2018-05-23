a3d359cf4f6a0b6d1914d021e331023183efb909
George <George@DESKTOP-24NTT0J>
2018-05-23 12:42:22 -0400

    Job Application and Profile

--- CareerCloud.Pocos/ApplicantEducationPoco.cs
+++ CareerCloud.Pocos/ApplicantEducationPoco.cs
@@ -20,15 +20,15 @@ namespace CareerCloud.Pocos
         public string Major { get; set; }
         [Column("Certificate_Diploma")]
         public string CertificateDiploma { get; set; }
-        [Column ("Start_Date")]
+        [Column("Start_Date")]
         public DateTime? StartDate { get; set; }
         [Column("Completion_Date")]
         public DateTime? CompletionDate { get; set; }
         [Column("Completion_Percent")]
         public byte? CompletionPercent { get; set; }
-        [Column ("Time_Stamp")]
-       public byte? TimeStamp { get; set; }
-
-
+        [Column("Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+        
     }
 }
+ 
